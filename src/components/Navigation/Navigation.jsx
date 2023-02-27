f0a80db140c4d09da90664bc5a73c1da43f74b20
Andrey Lavryonov <kedr_club@mail.ru>
2023-02-27 16:51:20 +0300

    feat: adds redux

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,63 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
 import cn from 'classnames';
 import styles from './Navigation.module.css';
 import Container from '../Container/Container';
+import { changeCategory } from '../../store/category/categorySlice';
 
 const Navigation = () => {
+  const { category, activeCategory } = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
   return (
     <nav className={styles.navigation}>
       <Container className={styles.container}>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <button
-              className={cn(
-                styles.button,
-                styles.button_burger,
-                styles.button_active
-              )}
-            >
-              Бургеры
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_snack)}>
-              Закуски
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_hotdog)}>
-              Хот-доги
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_combo)}>
-              Комбо
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_shawarma)}>
-              Шаурма
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_pizza)}>
-              Пицца
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_wok)}>
-              Вок
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_dessert)}>
-              Десерты
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={cn(styles.button, styles.button_sauce)}>
-              Соусы
-            </button>
-          </li>
+          {category.map((item, i) => (
+            <li className={styles.item} key={i}>
+              <button
+                className={cn(
+                  styles.button,
+                  activeCategory === i ? styles.button_active : ''
+                )}
+                style={{ backgroundImage: `url(${item.image})` }}
+                onClick={() => {
+                  dispatch(changeCategory);
+                }}
+              >
+                {item.rus}
+              </button>
+            </li>
+          ))}
         </ul>
       </Container>
     </nav>
