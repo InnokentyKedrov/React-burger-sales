8e07cacbb2542ede9b72ccdd2a80196693352e0e
Andrey <kedr_club@mail.ru>
2023-04-17 22:33:51 +0300

    fix: add grams

--- src/components/CatalogProduct/CatalogProduct.jsx
+++ src/components/CatalogProduct/CatalogProduct.jsx
@@ -1,7 +1,7 @@
-import { useDispatch } from 'react-redux';
-import { API_URI } from '../../const';
-import { addProduct } from '../../store/order/orderSlice';
-import styles from './CatalogProduct.module.css';
+import { useDispatch } from "react-redux";
+import { API_URI } from "../../const";
+import { addProduct } from "../../store/order/orderSlice";
+import styles from "./CatalogProduct.module.css";
 
 const CatalogProduct = ({ item }) => {
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ const CatalogProduct = ({ item }) => {
         <button className={styles.detail}>{item.title}</button>
       </h3>
 
-      <p className={styles.weight}>{item.weight}</p>
+      <p className={styles.weight}>{item.weight} г</p>
 
       <button
         className={styles.add}
